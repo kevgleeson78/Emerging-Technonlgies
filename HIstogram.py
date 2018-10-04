80caf1a6658142dab91f09b5d70d3f0fbb4c049a
Kevin <Kevin@DESKTOP-12A507O>
2018-10-04 12:51:52 +0100

    Bins param added

--- HIstogram.py
+++ HIstogram.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.random.normal(0.0, 1.0, 1000)
-
-plt.hist(x)
+# bins param to have more fine grain chart
+plt.hist(x, bins=40)
 
 plt.show()
